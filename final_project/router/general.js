58701732ee9e3cfd9ad3ec1080a7b92f580440e5
Suthar-Dev <suthardev1203@gmail.com>
2023-08-11 12:59:37 +0530

    Updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,32 +32,36 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  let filtered_books = books.filter((books) => books.isbn === isbn);
-  res.send(filtered_books);
+  const bookss = Object.values(books);
+  let filtered_books = bookss.filter((books) => books.isbn === isbn);
+  res.send(JSON.stringify(filtered_books));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  let filtered_books = books.filter((books) => books.author === author);
-  res.send(filtered_books);
+  const bookss = Object.values(books);
+  let filtered_books = bookss.filter((book) => book.author === author);
+  res.send(JSON.stringify(filtered_books));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
-  let filtered_books = books.filter((books) => books.title === title);
-  res.send(filtered_books);
+  const bookss = Object.values(books);
+  let filtered_books = bookss.filter((books) => books.title === title);
+  res.send(JSON.stringify(filtered_books));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const review = req.params.review;
-  let filtered_books = books.filter((books) => books.review === isbn);
-  res.send(filtered_books);
+  const bookss = Object.values(books);
+  let filtered_books = bookss.filter((books) => books.review === isbn);
+  res.send(JSON.stringify(filtered_books));
 });
 
 module.exports.general = public_users;
